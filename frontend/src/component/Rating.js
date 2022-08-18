fa6df330f920e6518747388e38c41b795ae2db03
selinaliu0626 <selinaliu0626@gmail.com>
2022-08-17 19:18:06 -0700

    version1

---
+++ frontend/src/component/Rating.js
@@ -0,0 +1,39 @@
+
+function Rating(props){
+    const {rating, numberReviews,caption} = props;
+    return (
+        <div className= "rating">
+            <span>
+                <i className={rating >=1? 'fas fa-star':
+                rating>=0.5 ? 'fas fa-star-half-alt': 'far fa-star'
+                } />
+            </span>
+            <span>
+                <i className={rating >=2? 'fas fa-star':
+                    rating>=1.5 ? 'fas fa-star-half-alt': 'far fa-star'
+                } />
+            </span>
+            <span>
+                <i className={rating >=3? 'fas fa-star':
+                    rating>=2.5 ? 'fas fa-star-half-alt': 'far fa-star'
+                } />
+            </span>
+            <span>
+                <i className={rating >=4? 'fas fa-star':
+                    rating>=3.5 ? 'fas fa-star-half-alt': 'far fa-star'
+                } />
+            </span>
+            <span>
+                <i className={rating >=5? 'fas fa-star':
+                    rating>=4.5 ? 'fas fa-star-half-alt': 'far fa-star'
+                } />
+            </span>
+            {caption ? (
+                <span>{caption}</span>
+            ) : (
+                <span>{' ' + numberReviews + ' reviews'}</span>
+            )}
+        </div>
+    )
+}
+export default Rating;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,74 @@
+
+// import data from '../data';
+import {useEffect, useReducer, useState} from "react";
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Product from "../component/Product";
+import {Helmet} from "react-helmet-async";
+import LoadingBox from "../component/LoadingBox";
+import MessageBox from "../component/MessageBox";
+
+const reducer = (state,action) =>{
+    switch (action.type){
+        case 'FETCH_REQUEST':
+            return {...state,loading:true};
+        case'FETCH_SUCCESS':
+            return {...state,products: action.payload, loading:false};
+        case'FETCH_FAIL':
+            return {...state,loading: false, error:action.payload};
+        default:
+            return state;
+    }
+}
+
+function HomeScreen() {
+    // const[products,setProducts] = useState([]);
+    const[{loading,error,products},dispatch] = useReducer(logger(reducer),{
+        products:[],
+        loading:true,
+        error:'',
+    });
+    useEffect(() =>{
+        const fetchData = async () => {
+            dispatch({type:'FETCH_REQUEST'});
+            try{
+                const result = await axios.get('/api/products');
+                dispatch({type:'FETCH_SUCCESS',payload:result.data});
+            }catch (err) {
+                dispatch({
+                    type: 'FETCH_FAIL', payload: err.message
+                });
+                const result = await axios.get('./api/products');
+                // setProducts(result.data);
+            }
+        };
+        fetchData();
+    },[]);
+  return (
+    <div>
+      <Helmet>
+          <title>A-buy</title>
+      </Helmet>
+        <h1>Featured Products</h1>
+      <div className="products">
+          {
+              loading? (<LoadingBox></LoadingBox>)
+                  :
+                  error? (<MessageBox variant="danger">{error}</MessageBox>)
+                      :(
+        <Row>
+            {products.map((product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+
+                </Col>
+        ))}
+        </Row>
+       )}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
